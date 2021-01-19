ced2bffa3fb000a4ea03098ffa03f51be1c572f2
Ivanka Delova (A1 BG) <Ivanka.Delova@a1.bg>
2021-01-19 14:07:55 +0200

    Initializing an ApolloClient instance

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+const client = new ApolloClient({ uri: 'https://nx9zvp49q7.lp.gql.zone/graphql' });
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>
 );
+
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
